a7b87d404193b318e5bd278dc13b27672de4e536
cvidaal <carlosvsedet@gmail.com>
2024-10-27 13:22:54 +0100

    Guardar estado y pestañas favoritas

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,116 +1,30 @@
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../providers/providers.dart';
+
+import '../../views/views.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
+  final int pageIndex;
 
-  const HomeScreen({super.key});
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    SizedBox(), // Categorias View
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
+    return  Scaffold(
+      body: IndexedStack( // <--- Este widget sirve para guardar el estado de la página
+        index: pageIndex,
+        children: viewRoutes, // Recibe [0, 1, 2] dependiendo de la página que muestra
+      ), 
+      bottomNavigationBar: CustomBottomNavigation(currentIndex: pageIndex),
     );
   }
 }
 
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
 
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    final initialLoading = ref.watch(initialLoadingProvider);
-    if(initialLoading) return const FullScreenLoader();
-
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-    final slideShowMovies = ref.watch(moviesSlideshowProvider);
-
-    return CustomScrollView(
-      slivers: [
-        const SliverAppBar( // Es lo mismo que un appbar
-          floating: true, 
-          flexibleSpace: FlexibleSpaceBar(
-            title: CustomAppbar(),
-            titlePadding: EdgeInsets.zero, // Quita el pading que aparece en la AppBar
-          ),
-          // Cuando deslizamos hacia abajo aparece
-        ),
-
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-
-            // const CustomAppbar(),
-
-            MoviesSlideshow(movies: slideShowMovies),
-            MoviesHorizontalListview(
-              movies: nowPlayingMovies,
-              title: 'En cines',
-              subTitle: 'Lunes 20',
-              loadNextPage: () {
-                ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MoviesHorizontalListview(
-              movies: upcomingMovies,
-              title: 'Próximamente',
-              subTitle: 'Este mes',
-              loadNextPage: () {
-                ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MoviesHorizontalListview(
-              movies: popularMovies,
-              title: 'Populares',
-              // subTitle: 'Este mes',
-              loadNextPage: () {
-                ref.read(popularMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MoviesHorizontalListview(
-              movies: topRatedMovies,
-              title: 'Mejor calificadas',
-              // subTitle: 'Este mes',
-              loadNextPage: () {
-                ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            )
-          ],
-        );
-      },
-              childCount:
-                  1)) // delegate es la función que sirve para crear los widgets dentro de este view
-    ]);
-  }
-}
